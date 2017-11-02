f0048dd1edc53e698a9a7f3d80dc9e81c265c529
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-11-02 13:52:01 +0200

    Add 55op measure even via different path.

--- db/v201711/11__c-koulutus_vuosi-korkeakoulu.sql
+++ db/v201711/11__c-koulutus_vuosi-korkeakoulu.sql
@@ -10,8 +10,10 @@ SELECT vuosi
 , sum(opiskelijat_fte) as opiskelijat_fte
 , sum(opiskelijat_lasna) as opiskelijat_lasna
 , sum(tutkinnot) as tutkinnot
+, (select sum(hei_bullet.arvo) from hei_bullet
+    where hei_bullet.oppilaitos=koulutus_perus_kk_ala_vuosi.organisaatio_koodi and cast(hei_bullet.vuosi as int)=koulutus_perus_kk_ala_vuosi.vuosi
+    and hei_bullet.mittari='opiskelijat_viisviis') AS opiskelijat_viisviis
 FROM koulutus_perus_kk_ala_vuosi
---WHERE organisaatio_koodi='02609' AND vuosi=2016
 group by vuosi, organisaatio_koodi, organisaatio, organisaatio_sv, organisaatio_en
 order by 1,2
 ;
