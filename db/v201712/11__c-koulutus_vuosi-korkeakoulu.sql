ba318929d4bd32f44601c911edc1a209c08ace10
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-11-06 16:26:12 +0200

    Include pass rate (lapaisy4v) to graph data.

--- db/v201712/11__c-koulutus_vuosi-korkeakoulu.sql
+++ db/v201712/11__c-koulutus_vuosi-korkeakoulu.sql
@@ -13,6 +13,9 @@ SELECT vuosi
 , (select sum(hei_bullet.arvo) from hei_bullet
     where hei_bullet.oppilaitos=koulutus_perus_kk_ala_vuosi.organisaatio_koodi and cast(hei_bullet.vuosi as int)=koulutus_perus_kk_ala_vuosi.vuosi
     and hei_bullet.mittari='opiskelijat_viisviis') AS opiskelijat_viisviis
+, (select sum(hei_bullet.arvo) from hei_bullet
+    where hei_bullet.oppilaitos=koulutus_perus_kk_ala_vuosi.organisaatio_koodi and cast(hei_bullet.vuosi as int)=koulutus_perus_kk_ala_vuosi.vuosi
+    and hei_bullet.mittari='lapaisy4v') AS lapaisy4v
 FROM koulutus_perus_kk_ala_vuosi
 group by vuosi, organisaatio_koodi, organisaatio, organisaatio_sv, organisaatio_en
 order by 1,2
